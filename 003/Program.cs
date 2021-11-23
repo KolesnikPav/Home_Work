2acf32be3df1391258d5cce27166f66550d49bac
Pavel <kolesnik_pasha_9@mail.ru>
2021-11-23 16:34:45 +0300

    done

---
+++ 003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+Console.Write("Введите первое число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine(a + " > " + b);
+}
+if (a < b)
+{
+    Console.WriteLine(a + " < " + b);
+}
+if (a == b)
+{
+    Console.WriteLine("Числа равны!");
+}
