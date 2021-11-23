2e484240874705e652025fc6670abbb1440a7fd8
Pavel <kolesnik_pasha_9@mail.ru>
2021-11-23 18:57:15 +0300

    done

---
+++ 004/Program.cs
@@ -0,0 +1,23 @@
+﻿// По заданному номеру дня недели вывести его название
+
+string[] array = 
+{
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+    "Воскресенье"
+};
+Console.WriteLine("Введите номер дня недели: ");
+int day = int.Parse(Console.ReadLine());
+
+if (day > 7 || day < 1)
+{
+    Console.WriteLine("В неделе 7 дней!");
+}
+else
+{
+    Console.WriteLine(array [day-1]);
+}
