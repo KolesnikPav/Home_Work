9396f4085590873e372eaa46165fb9936fc92950
Pavel <kolesnik_pasha_9@mail.ru>
2021-11-23 14:45:09 +0300

    done

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,3 +1,5 @@
-﻿Console.Write("Введите число, которое необходимо возвести в квадрат: ");
+﻿// Вывести квадрат числа
+
+Console.Write("Введите число, которое необходимо возвести в квадрат: ");
 string a = Console.ReadLine();
 Console.WriteLine(a + " в квадрате = " + Convert.ToDouble(a) * Convert.ToDouble(a));
