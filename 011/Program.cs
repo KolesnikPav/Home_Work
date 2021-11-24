60f9a15567f1df6b9281f0a1871573ec26a1828c
Pavel <kolesnik_pasha_9@mail.ru>
2021-11-24 15:19:38 +0300

    done

---
+++ 011/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Write("Введите трехзначное число: ");
+string a = Console.ReadLine();
+
+if(int.Parse(a) / 10 >= 10)
+{
+    if(int.Parse(a) / 10 <= 99)
+    {
+        Console.WriteLine(a[1]);
+        }
+}
