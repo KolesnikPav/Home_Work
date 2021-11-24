b798c572e6d232a8b779a902911312569531ea44
Pavel <kolesnik_pasha_9@mail.ru>
2021-11-24 09:39:01 +0300

    done

---
+++ 006/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+
+Console.Write("Введите чему будет равно 'a': ");
+int a = int.Parse(Console.ReadLine());
+
+if(a >= 0)
+{
+    Console.WriteLine("y = " + (a - 1));
+}
+else
+{
+    Console.WriteLine("y = " + Math.Abs(a));
+}
