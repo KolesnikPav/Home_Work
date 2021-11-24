ad11ea6e9c06c536dd8aee174ab4bd280f7bbc0a
Pavel <kolesnik_pasha_9@mail.ru>
2021-11-24 14:19:18 +0300

    done

---
+++ 009/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать четные числа от 1 до N
+
+Console.Write("Введите значение 'N': ");
+int a = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+while(count <= (a - 2))
+{
+    count = count + 2;
+    Console.WriteLine(count);
+}
