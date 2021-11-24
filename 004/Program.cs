de36e9e694d4f8f54a1ef09ee470fa52943c9136
Pavel <kolesnik_pasha_9@mail.ru>
2021-11-24 09:40:45 +0300

    done

--- 004/Program.cs
+++ 004/Program.cs
@@ -10,7 +10,7 @@ string[] array =
     "Суббота",
     "Воскресенье"
 };
-Console.WriteLine("Введите номер дня недели: ");
+Console.Write("Введите номер дня недели: ");
 int day = int.Parse(Console.ReadLine());
 
 if (day > 7 || day < 1)
