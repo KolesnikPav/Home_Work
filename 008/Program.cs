69c989892241f7ebf6faa6f103158a5b93d70e59
Pavel <kolesnik_pasha_9@mail.ru>
2021-11-25 19:08:59 +0300

    done

---
+++ 008/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать числа от -N до N
+
+Console.Write("Введите значение 'N': ");
+int a = int.Parse(Console.ReadLine());
+int count = 1;
+Console.WriteLine(0);
+while(count <= a)
+{
+    Console.WriteLine(count - 2 * count);
+    Console.WriteLine(count);
+    count++;
+}
