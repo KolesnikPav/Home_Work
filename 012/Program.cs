7406010b48a659902137bb9a55c14072dcc5734d
Pavel <kolesnik_pasha_9@mail.ru>
2021-11-25 22:24:41 +0300

    done

--- 012/Program.cs
+++ 012/Program.cs
@@ -2,12 +2,13 @@
 
 Console.Write("Введите любое целое число в диапазоне [10, 99]: ");
 string a = Console.ReadLine();
+int b = int.Parse(a);
 
-if(int.Parse(a) <= 9 || int.Parse(a) >= 100)
+if(b <= 9 || b >= 100)
 {
     Console.WriteLine("Число от 10 до 99. Давай еще раз!");
 }
-    else if(a[0] > a[1])
+    else if(int.Parse("" + a[0]) > int.Parse("" + a[1]))
     {
         Console.WriteLine("В числе " + a + " наибольшая цифра: " + a[0]);
     }
