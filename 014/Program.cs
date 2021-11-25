fcf803cedbdcd975bd02ebcc8f88665547947e44
Pavel <kolesnik_pasha_9@mail.ru>
2021-11-25 18:00:21 +0300

    done

---
+++ 014/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток
+
+Console.Write("Введите первое число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+if (a % b == 0)
+{
+    Console.WriteLine(a + " кратно " + b);
+}
+else
+{
+    Console.WriteLine("Остаток: " + a % b);
+}
