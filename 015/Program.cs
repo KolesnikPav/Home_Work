a288c805ba87c7784234eadf50dadd1eb9c17bd8
Pavel <kolesnik_pasha_9@mail.ru>
2021-11-25 17:49:52 +0300

    done

---
+++ 015/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.Write("Введите число: ");
+string a = Console.ReadLine();
+
+if(int.Parse(a) / 10 <= 9)
+{
+    Console.WriteLine("Третьей цифры в числе нет!");
+    //else if(int.Parse(a) / 10 >= 100)
+
+}
+else
+{
+    Console.WriteLine(a[2]);
+}
