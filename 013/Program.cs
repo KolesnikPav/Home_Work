f6bd33cbcd03a51bb511b1e44875d6eecbf0ffd4
Pavel <kolesnik_pasha_9@mail.ru>
2021-11-25 17:42:34 +0300

    done

---
+++ 013/Program.cs
@@ -0,0 +1,14 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.Write("Введите трехзначное число: ");
+string a = Console.ReadLine();
+
+if(int.Parse(a) / 10 <= 9 || int.Parse(a) / 10 >= 100)
+{
+    Console.WriteLine("Я сказал трёхзначное!");
+}
+else
+{
+    Console.WriteLine(Convert.ToString(a[0]) + Convert.ToString(a[2]));
+}
+
